9f9578c624ad50ead360201cc95bc2d4df2e5968
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-11-16 16:13:22 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-void main() => runApp(ProgrammaticAdjacentDate());
+void main() => runApp(const ProgrammaticAdjacentDate());
 
 class ProgrammaticAdjacentDate extends StatefulWidget {
+  const ProgrammaticAdjacentDate({super.key});
+
   @override
   AdjacentDateState createState() => AdjacentDateState();
 }
 
 class AdjacentDateState extends State<ProgrammaticAdjacentDate> {
-  final CalendarController? _controller = CalendarController();
+  final CalendarController _controller = CalendarController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,43 +19,43 @@ class AdjacentDateState extends State<ProgrammaticAdjacentDate> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: SafeArea(
-        child: Column(
-          children: [
-            Row(
+            child: Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(30, 20, 0, 20),
-                  child: RaisedButton.icon(
-                    icon: Icon(Icons.arrow_back),
-                    label: Text('Backward'),
-                    onPressed: () {
-                      _controller!.backward!();
-                    },
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(30, 20, 0, 20),
+                      child: TextButton.icon(
+                        icon: const Icon(Icons.arrow_back),
+                        label: const Text('Backward'),
+                        onPressed: () {
+                          _controller.backward!();
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(70, 20, 0, 20),
+                      child: TextButton.icon(
+                        label: const Text('Forward'),
+                        icon: const Icon(Icons.arrow_forward),
+                        onPressed: () {
+                          _controller.forward!();
+                        },
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: SfCalendar(
+                    controller: _controller,
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(70, 20, 0, 20),
-                  child: RaisedButton.icon(
-                    label: Text('Forward'),
-                    icon: Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      _controller!.forward!();
-                    },
-                  ),
-                )
               ],
             ),
-            Expanded(
-              child: SfCalendar(
-                controller: _controller,
-              ),
-            ),
-          ],
-        ),
-      )
+          )
 
-          // This trailing comma makes auto-formatting nicer for build methods.
-          ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
